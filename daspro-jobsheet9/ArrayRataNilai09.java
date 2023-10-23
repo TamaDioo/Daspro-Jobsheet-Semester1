9b2875ab837c007fbc52c9f317a5c10b9376da66
Tama Dioo <tamadio91@gmail.com>
2023-10-23 15:38:52 +0700

    Modifikasi percobaan 3 jobsheet 9

--- daspro-jobsheet9/ArrayRataNilai09.java
+++ daspro-jobsheet9/ArrayRataNilai09.java
@@ -5,11 +5,15 @@ public class ArrayRataNilai09 {
     //Deklarasi Scanner
     Scanner sc = new Scanner(System.in);
 
-    //Deklarasi array bertipe integer dengan nama nilaiMhs berkapasitas 10 elemen.
-    int[] nilaiMhs = new int[10];
+    System.out.print("Masukkan jumlah mahasiswa: ");
+    int jumlah_Mhs = sc.nextInt();
+
+    //Deklarasi array bertipe integer dengan nama nilaiMhs.
+    int[] nilaiMhs = new int[jumlah_Mhs];
     //Deklarasi variable total dan rata2 bertipe double.
-    double total = 0;
-    double rata2;
+    double total = 0, totalMhslls = 0, totalMhstdklls = 0;
+    double rata2lls, rata2tdklls;
+    int mhsLulus = 0, mhsTdkLls = 0;
 
     //Struktur perulangan untuk meminta input untuk mengisi array nilaiMhs.
     for (int i = 0; i < nilaiMhs.length; i++) {
@@ -19,12 +23,22 @@ public class ArrayRataNilai09 {
     //Struktur perulangan untuk menghitung jumlah keseluruhan nilai dalam array nilaiMhs
     for (int i = 0; i < nilaiMhs.length; i++) {
         total += nilaiMhs[i];
+        if (nilaiMhs [i] > 70) {
+            totalMhslls += nilaiMhs [i];
+            mhsLulus++;
+        } else {
+            totalMhstdklls += nilaiMhs [i];
+        }
     }
 
-    //Menghitung nilai rata2 dengan cara nilai total dibagi jumlah elemen dari array nilaiMhs.
-    rata2 = total/nilaiMhs.length;
-    //Menampilkan rata-rata nilai.
-    System.out.println("Rata-rata nilai ="+rata2);
+    //Menghitung nilai rata2 nilai mahasiswa yang lulus.
+    rata2lls = totalMhslls/mhsLulus;
+    //Menampilkan rata-rata nilai mahasiswa yang lulus.
+    System.out.println("Rata-rata nilai mahasiswa yang lulus = " + rata2lls);
+    //Menghitung nilai rata2 nilai mahasiswa yang lulus.
+    rata2tdklls = totalMhstdklls/(nilaiMhs.length-mhsLulus);
+    //Menampilkan rata-rata nilai mahasiswa yang tidak lulus.
+    System.out.println("Rata-rata nilai mahasiswa yang lulus = " + rata2tdklls);
     
     }
 }
