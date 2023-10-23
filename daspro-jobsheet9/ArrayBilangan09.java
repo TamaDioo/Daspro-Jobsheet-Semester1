93047671c6829b2ae5c71e1e6336986d73cd0041
Tama Dioo <tamadio91@gmail.com>
2023-10-23 13:54:05 +0700

    Modifikasi kode program percobaan 1 jobsheet 9

--- daspro-jobsheet9/ArrayBilangan09.java
+++ daspro-jobsheet9/ArrayBilangan09.java
@@ -2,17 +2,12 @@ public class ArrayBilangan09 {
    public static void main(String args []) {
     
         //Deklarasi array bertipe integer dengan nama bil berkapasitas 4 elemen.
-        int[] bil = new int[4];
-        //Mengisi elemen array bil dengan angka 5, 13, -7, 17.
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
+        // inisialisasi elemen array sekaligus dengan menambahkan kurung kurawal.
+        int[] bil = {5, 13, -7, 17};
 
         //Menampilkan isi elemen array ke layar.
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+       for (int i = 0; i <= 4; i++) {
+        System.out.println(bil[i]);
+       }
     }
 }
