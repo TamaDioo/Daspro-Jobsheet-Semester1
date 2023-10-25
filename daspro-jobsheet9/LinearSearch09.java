7075bd9b2f729695ab7143e701e1f4b6c4d74e93
Tama Dioo <tamadio91@gmail.com>
2023-10-25 12:05:36 +0700

    Modifikasi percobaan 4 (revisi) jobsheet 9

--- daspro-jobsheet9/LinearSearch09.java
+++ daspro-jobsheet9/LinearSearch09.java
@@ -1,18 +1,51 @@
+import java.util.Scanner;
+
 public class LinearSearch09 {
     public static void main(String[] args) {
-        
-        int[] arrayInt = {34, 18, 226, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
 
+        // Deklarasi Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Meminta inputan jumlah elemen kepada user.
+        System.out.print("Masukkan jumlah elemen array: ");
+        int jml_Elemen = sc.nextInt();
+
+        // Instansiasi array berdasarkan jumlah elemen yang telah dimasukkan user.
+        int[] arrayInt = new int[jml_Elemen];
+
+        // Struktur perulangan untuk meminta nilai setiap elemen.
         for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen array ke-" + i + " : ");
+            arrayInt[i] = sc.nextInt();
+        }
+
+        // Deklarasi dan inisialisasi variabel hasil untuk menyimpan indeks
+        int hasil = 0;
+        int i;
+        //Menambahkan variabel boolean untuk pengecekan apakah key ditemukan
+        //Diinisialisasi dengan false
+        boolean ditemukan = false; 
+
+        // Meminta inputan key yang ingin dicari user
+        System.out.print("Masukkan key yang ingin dicari: ");
+        int key = sc.nextInt();
+
+        // Streuktur perulangan dan if untuk mencari key pada setiap elemen.
+        for (i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
-                break;
+                //Jika key ditemukan maka variabel boolean menjadi true.
+                ditemukan = true; 
+                break; // Jika menemukan key akan keluar dari loop.
             }
         }
 
-        System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
-
+        // Struktur if else untuk menentukan output jika key ditemukan/tidak.
+        if (ditemukan == true) {
+            System.out.println("Key ada di posisi indeks ke-" + hasil);
+        } else {
+            //Jika tidak ditemukan blok else akan dieksekusi.
+            System.out.println("Key tidak ditemukan");
+        }
     }
 }
