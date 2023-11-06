13c9216593a66656e751f4d1cf3411426222a14d
Tama Dioo <tamadio91@gmail.com>
2023-11-06 17:06:47 +0700

    Push dan commit tugas individu nomor 1

--- daspro-jobsheet11/Individu109.java
+++ daspro-jobsheet11/Individu109.java
@@ -6,6 +6,12 @@ public class Individu109 {
         System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
         
+         // Memastikan nilai N < 3
+         if (N < 3) {
+            System.out.println("Nilai N minimal 3!");
+            return;
+        }
+
         //Mencetak tampilan segitiga angka
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N - i; j++) {
