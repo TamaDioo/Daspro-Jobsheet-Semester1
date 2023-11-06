5319af58a9440cb71a57f5a8f57f9b26d6a99b20
Tama Dioo <tamadio91@gmail.com>
2023-11-06 16:55:41 +0700

    Push dan commit tugas individu nomor 3

---
+++ daspro-jobsheet11/Individu309.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Individu309 {
+    public static void main(String[] args) {
+        Scanner sc27 = new Scanner(System.in);
+        System.out.print("Masukkan nilai N: ");
+        int N = sc27.nextInt();
+
+         // Memastikan nilai N < 3
+         if (N < 3) {
+            System.out.println("Nilai N minimal 3!");
+            return;
+        }
+
+
+        //Mencetak tampilan persegi angka 
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == 1 || i == N || j == 1 || j == N) {
+                    System.out.print(N+" ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
