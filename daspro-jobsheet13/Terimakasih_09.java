119ea54af793216768e73ff468df5b3b4d4539dc
Tama Dioo <tamadio91@gmail.com>
2023-11-20 11:58:26 +0700

    Percobaan 1 jobsheet 13 Fungsi 1

---
+++ daspro-jobsheet13/Terimakasih_09.java
@@ -0,0 +1,11 @@
+public class Terimakasih_09 {
+    //Membuat fungsi UcapanTerimaKasih
+    public static void UcapanTerimaKasih() {
+        System.out.println("Thank you for being the best teacher in the world.\n" +
+        "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+
+    public static void main(String[] args) {
+        UcapanTerimaKasih();//Memanggil fungsi UcapanTerimaKasih
+    }
+}
